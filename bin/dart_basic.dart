54d46fd8f50cf4c77d5a45815eff414d4acf1223
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-08-05 19:11:24 +0700

    switch

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -3,17 +3,18 @@ import 'dart:io';
 void main(List<String> arguments) {
   print('Masukkan angka: ');
   int number = int.tryParse(stdin.readLineSync());
-  String output;
 
-  // if (number > 0) {
-  //   print('positif');
-  // } else if (number < 0) {
-  //   print('negatif');
-  // } else {
-  //   print('nol');
-  // }
-
-  output = (number > 0) ? 'positif' : 'negative atau nol';
-
-  print(output);
+  switch (number) {
+    case 0:
+      print('nol');
+      break;
+    case 1:
+      print('satu');
+      break;
+    case 2:
+      print('dua');
+      break;
+    default:
+      print('bilangan lain');
+  }
 }
