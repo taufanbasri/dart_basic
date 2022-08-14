f2e0660fb907899431adaf88c1b703982874d6e2
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-08-14 19:54:16 +0700

    for looping

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,20 +1,7 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Masukkan angka: ');
-  int number = int.tryParse(stdin.readLineSync());
-
-  switch (number) {
-    case 0:
-      print('nol');
-      break;
-    case 1:
-      print('satu');
-      break;
-    case 2:
-      print('dua');
-      break;
-    default:
-      print('bilangan lain');
+  for (int counter = 0; counter < 5; counter++) {
+    print('hallo');
   }
 }
