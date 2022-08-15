73638a213b7b89b774a1180519ed291e80ae1c3d
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-08-15 17:49:50 +0700

    List/Array

---
+++ bin/list.dart
@@ -0,0 +1,30 @@
+void main(List<String> args) {
+  List<int> myList = [];
+  List<int> list = [1, 2, 3];
+
+  for (int bilangan in list) {
+    print(bilangan);
+  }
+
+  myList.add(10); // menambahkan data ke array
+  myList.addAll(list); // menambahkan keseluruhan data dalam array
+  myList.insert(1, 22); // menyisipkan data ke index tertentu
+  myList.insertAll(2, [33, 44, 55]);
+  // menghapus nilai 22, jika ada yang sama index terkecil yang akan dihapus
+  myList.remove(22);
+  myList.removeLast();
+  myList.removeAt(1); // remove index ke-
+  // remove index 0 s/d 2, index ke-3 sebagai batas akhir
+  myList.removeRange(0, 3);
+
+  // menghapus semua angka ganjil
+  myList.removeWhere((number) => number % 2 != 0);
+
+  if (myList.contains(2)) {
+    print('betul');
+  }
+
+  myList.forEach((bilangan) {
+    print(bilangan);
+  });
+}
