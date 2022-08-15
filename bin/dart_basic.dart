161c19f6a398f81b1cc3275267184595795347b4
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-08-15 11:50:40 +0700

    Lambda Expression

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -8,7 +8,11 @@ String say2(String from, String message, [String to, String appName]) {
   return "$from say $message ${(to != null) ? "to $to" : ""} ${(appName != null) ? "via $appName" : ""}";
 }
 
+double luas_segiempat(double panjang, double lebar) => panjang * lebar;
+
 void main(List<String> arguments) {
   print(say('taufan', 'hello world', to: 'evita'));
   print(say2('taufan', 'hello world', 'evita', 'whatsapp'));
+
+  print(luas_segiempat(10, 5));
 }
