8704f9026473efe00027a847bfee1a7149fd499d
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-08-18 09:49:17 +0700

    enum

---
+++ bin/enum_cascade.dart
@@ -0,0 +1,32 @@
+void main(List<String> args) {
+  MonsterUcoa monster = MonsterUcoa(status: UcoaStatus.confused);
+
+  monster.move();
+}
+
+enum UcoaStatus { normal, poisoned, confused }
+
+class MonsterUcoa {
+  final UcoaStatus status;
+
+  MonsterUcoa({this.status = UcoaStatus.normal});
+
+  void move() {
+    switch (status) {
+      case UcoaStatus.normal:
+        print('Ucoa is moving');
+        break;
+      case UcoaStatus.poisoned:
+        print('Ucoa cannot move. Ucoa is dying. Ucoa need help.');
+        break;
+      case UcoaStatus.confused:
+        print('Ucoa is spinning. Dart language is confusing.');
+        break;
+      default:
+    }
+  }
+
+  void eat() {
+    print('Ucoa is eating');
+  }
+}
